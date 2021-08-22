cb950b3163217ca3d5cecc68d9e67b7b1a4bdae6
saruulkhasar <88153700+saruulkhasar@users.noreply.github.com>
2021-08-21 19:04:26 -0600

    Initial Commit

---
+++ jquery/index.js
@@ -0,0 +1,3 @@
+// alert("Working");
+
+$("h1").addClass("big-title");      // change color property to red using jQuery
---
+++ js/index.js
@@ -0,0 +1,18 @@
+// alert("JS file is linked!")
+// -- this is to see if the .js file is linked
+
+//////////////  smoothly change pages  /////////////////////
+
+
+
+
+//////////////  all links at one place /////////////////////
+
+var myImg = "images/my_picture.jpg";
+
+// Project page links:
+var img6 = "images/projects/6.png";
+var img5 = "images/projects/5.png";
+// var randomNumber1 = Math.floor(Math.random()*7) ;   /* creates random numbers between 1 to 6 */
+// var randomDiceImage = "images/dice"+randomNumber1+".png"
+var img4 = "images/projects/4.png";
