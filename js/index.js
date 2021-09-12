cff46de201e912639e3c02bb56e9cce6e9d3f26d
Saruul <55937196+saruulkh5@users.noreply.github.com>
2021-09-11 18:38:02 -0600

    edited books section

--- js/index.js
+++ js/index.js
@@ -5,6 +5,25 @@ $(document).ready(function(){
   })
 })
 
+//////////////  books page  /////////////
+$(document).ready(function(){
+  $.get('ichiro-kishimi.html').done(function(data){
+    $('.ichiro-kishimi').html(data)
+  })
+})
+
+$(document).ready(function(){
+  $.get('john-bogle.html').done(function(data){
+    $('.john-bogle').html(data)
+  })
+})
+
+$(document).ready(function(){
+  $.get('phil-knight.html').done(function(data){
+    $('.phil-knight').html(data)
+  })
+})
+
 $(document).ready(function(){
   $.get('benjamin-graham.html').done(function(data){
     $('.benjamin-graham').html(data)
